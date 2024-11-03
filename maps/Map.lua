3da1d8eba87abac6e2bccb4fc1e739856956ebf2
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-02 20:02:55 -0400

    update on maps

--- maps/Map.lua
+++ maps/Map.lua
@@ -8,10 +8,16 @@ function Map:New(data, width, height)
         height = height,
         cellSize = 64
     }
-    --setmetatable(this,setfenv)
+    setmetatable(this,self)
     return this
 end
 
 function Map:Render()
-
+    for row = 0, self.height do
+        for col = 0, self.width do
+            local sx = col * self.cellSize
+            local sy = col * self.cellSize
+            love.graphics.rectangle("line", sx, sy, self.cellSize, self.cellSize)
+        end
+    end
 end
