44a8c1ebe349a3b7d9a0127ccca69490ec0a172c
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-03 20:00:42 -0500

    map imported

--- main.lua
+++ main.lua
@@ -3,8 +3,8 @@
 sti = require('libraries.sti')
 gameMap = sti('maps/gameMap.lua')
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 640
+-- SCREEN_WIDTH = 1024
+-- SCREEN_HEIGHT = 640
 
 -- local tiles = {
 --     0,0,1,0,0,0,0,0,
@@ -19,7 +19,7 @@ SCREEN_HEIGHT = 640
 --vlocal gMap = Map:New(tiles, 8, 6)
 
 function love.load()
-    love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, {vsync=true, fullscreen=false})
+  --  love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, {vsync=true, fullscreen=false})
 end
 
 function love.update(dt)
@@ -27,5 +27,5 @@ function love.update(dt)
 end
 
 function love.draw()
-    gameMap:draw()
+    gameMap:draw(0,0)
 end
