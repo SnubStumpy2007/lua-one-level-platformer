3458019ae9d91b795d2c8475ac7c72f6d23403c6
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-05 18:51:39 -0500

    working on player sprite and animations

--- main.lua
+++ main.lua
@@ -1,21 +1,33 @@
-sti = require('libraries.sti')
-gameMap = sti('maps/gameMap.lua')
-push = require('libraries.push-master.push')
-
 
 SCREEN_WIDTH, SCREEN_HEIGHT = love.window.getDesktopDimensions()
-SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_WIDTH * 0.8, SCREEN_HEIGHT * 0.8
+--CREEN_WIDTH, SCREEN_HEIGHT = SCREEN_WIDTH * 0.8, SCREEN_HEIGHT * 0.8
 
-VIRTUAL_WIDTH, VIRTUAL_HEIGHT = love.window.getDesktopDimensions()
--- readd 320, 240 back to the virtual height when camera is ready to work
+VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 790, 530
+-- readd 320, 240 back to the virtual height when camera is ready to work 790, 640
 
 function love.load()
+
+  -- libraries
+  sti = require('libraries.sti')
+  gameMap = sti('maps/gameMap.lua')
+  push = require('libraries.push-master.push')
+
+
+  -- importing modules
+  player = require('/modules/player')
+  walls = require('/modules/walls')
+
+
+  -- push setup for fullscreen game
   love.graphics.setDefaultFilter("nearest", "nearest")
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, {fullscreen = false, vsync = true, resizable = true})
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, {fullscreen = true, vsync = true, resizable = true, stretched = true})
 end
 
 function love.update(dt)
-    
+  --press the escape key to exit the game
+    if love.keyboard.isDown("escape") then
+      love.event.quit()
+    end
 end
 
 function love.resize(w,h)
@@ -25,5 +37,6 @@ end
 function love.draw()
   push:start()
     gameMap:draw(0,0)
+    love.graphics.draw(player.sprite, player.x, player.y)
     push:finish()
 end
---
+++ modules/player.lua
@@ -0,0 +1,7 @@
+local player = {}
+player.x = 100
+player.y = 450
+player.speed = 5
+player.sprite = love.graphics.newImage('/sprites/guido.png')
+
+return player
