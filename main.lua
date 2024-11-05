93f0e6d0f2597a80630c2e23b8eb32bb79a66721
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-04 19:36:10 -0500

    added player sprite

--- main.lua
+++ main.lua
@@ -6,12 +6,11 @@ push = require('libraries.push-master.push')
 SCREEN_WIDTH, SCREEN_HEIGHT = love.window.getDesktopDimensions()
 SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_WIDTH * 0.8, SCREEN_HEIGHT * 0.8
 
-VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 320, 240
-
+VIRTUAL_WIDTH, VIRTUAL_HEIGHT = love.window.getDesktopDimensions()
+-- readd 320, 240 back to the virtual height when camera is ready to work
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
-    --love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, {vsync=true, fullscreen=false})
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, {fullscreen = false, vsync = true, resizable = true})
 end
 
