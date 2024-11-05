05be30b35be9c80be9223944feaedd6901f05dd4
SnubStumpy2007 <joshua192007@gmail.com>
2024-11-04 19:12:47 -0500

    push library added to project

--- main.lua
+++ main.lua
@@ -1,31 +1,30 @@
---require "maps.map"
-
 sti = require('libraries.sti')
 gameMap = sti('maps/gameMap.lua')
+push = require('libraries.push-master.push')
 
--- SCREEN_WIDTH = 1024
--- SCREEN_HEIGHT = 640
 
--- local tiles = {
---     0,0,1,0,0,0,0,0,
---     0,1,1,0,0,0,0,0,
---     0,1,0,0,1,1,0,0,
---     0,0,0,0,1,1,0,0,
---     0,0,0,0,0,0,0,0,
---     0,0,0,0,0,0,0,0
+SCREEN_WIDTH, SCREEN_HEIGHT = love.window.getDesktopDimensions()
+SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_WIDTH * 0.8, SCREEN_HEIGHT * 0.8
 
--- }
+VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 320, 240
 
---vlocal gMap = Map:New(tiles, 8, 6)
 
 function love.load()
-  --  love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, {vsync=true, fullscreen=false})
+  love.graphics.setDefaultFilter("nearest", "nearest")
+    --love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, {vsync=true, fullscreen=false})
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, {fullscreen = false, vsync = true, resizable = true})
 end
 
 function love.update(dt)
     
 end
 
+function love.resize(w,h)
+  push:resize(w,h)
+end
+
 function love.draw()
+  push:start()
     gameMap:draw(0,0)
+    push:finish()
 end
